012366f44cec2a09ca7fca5e19ffe229fb5f54b6
eirslett <eiriksletteberg@gmail.com>
2015-07-18 18:50:38 +0200

    utility to convert a ListenableFuture into a Twitter Future

--- zipkin-common/src/main/scala/com/twitter/zipkin/common/JavaHelpers.scala
+++ zipkin-common/src/main/scala/com/twitter/zipkin/common/JavaHelpers.scala
@@ -1,5 +1,8 @@
 package com.twitter.zipkin.common
 
+import com.google.common.util.concurrent.{FutureCallback, Futures, ListenableFuture}
+import com.twitter.util.{Promise, Future}
+
 import scala.collection.JavaConverters._
 
 object JavaHelpers {
@@ -11,5 +14,14 @@ object JavaHelpers {
                binaryAnnotations: java.util.List[BinaryAnnotation],
                debug: Boolean) = {
     Span.apply(traceId, name, id, parentId, annotations.asScala.toList, binaryAnnotations.asScala, debug)
+  }
+
+  def mkTwitterFuture[T](future: ListenableFuture[T]) : Future[T] = {
+    val promise = new Promise[T]
+    Futures.addCallback(future, new FutureCallback[T] {
+      override def onSuccess(result: T) = promise.setValue(result)
+      override def onFailure(throwable: Throwable) = promise.setException(throwable)
+    })
+    promise
   }
 }
